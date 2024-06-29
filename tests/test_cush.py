3054243a4b0de32dc338162a2f0544be296a16e1
James Light <jl25019@gmail.com>
2024-06-29 10:34:55 -0400

    start with clean tests

--- tests/test_cush.py
+++ tests/test_cush.py
@@ -4,49 +4,25 @@ import cush
 from cush import __version__
 from thewired import Namespace
 
-def test_CushApplication_init():
-    c = cush.CushApplication(name='test1', namespace=Namespace())
-    assert c.name == 'test1'
+
+@pytest.fixture
+def CushApplication(request):
+    try:
+        app_name = request.param
+    except AttributeError:
+        app_name = ''
+
+    c = cush.CushApplication(name=app_name, namespace=Namespace())
+    yield c
+    c._applications.clear()
+    del c
+
+@pytest.mark.parametrize('name', ['test1', 'test2'])
+def test_CushApplication_name(name):
+    c = cush.CushApplication(name=name, namespace=Namespace())
+    assert c.name == name
 
 
 
-def test_init_userNS():
-    cushapp = cush.CushApplication(name='init_user_test', namespace=Namespace())
-    cushapp.init_user_namespace()
-    assert cushapp.name == 'init_user_test'
-
-
-
-#def test_init_implementorNS():
-#    # ATM only default application name is supported for initializing implementor NS
-#    #cushapp = cush.CushApplication(name='init_implementor_test', namespace=Namespace())
-#    cushapp = cush.CushApplication(name='default', namespace=Namespace())
-#    cushapp.init_user_namespace()
-#    cushapp.init_implementor_namespace()
-#    #assert cushapp.name == 'init_implementor_test'
-#    assert cushapp.name == 'default'
-#
-
-
-#def test_init_defaultNS():
-#    # ATM only default application name is supported for initializing implementor NS
-#    #cushapp = cush.CushApplication(name='init_implementor_test', namespace=Namespace())
-#    cushapp = cush.CushApplication(name='default', namespace=Namespace())
-#    cushapp.init_user_namespace()
-#    cushapp.init_implementor_namespace()
-#    cushapp.init_default_namespace()
-#    assert cushapp.name == 'default'
-
-@pytest.fixture(scope="module")
-def default_cush():
+def test_init_cush(CushApplication):
     cush.init_cush(step=False)
-    return cush.get_cush()
-
-def test_init_cush(default_cush):
-    assert default_cush.name == 'default'
-
-def test_provider_implementor_ns_get_root(default_cush):
-    root_node = default_cush._ns.root
-    provider_implementor_ns_root = root_node.provider.boto3.aws.s3.buckets.get._delegate.implementor_ns.get('.')
-    assert str(provider_implementor_ns_root.nsid) == '.'
-    assert str(provider_implementor_ns_root._delegate.nsid == '.implementor')
