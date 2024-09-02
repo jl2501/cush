1fe2b28360d90117f51b886f012ff58385891e85
James Light <jl25019@gmail.com>
2024-09-02 13:29:03 -0400

    add test showing the problem

--- tests/test_cush.py
+++ tests/test_cush.py
@@ -9,9 +9,44 @@ from thewired import Namespace
 def test_CushApplication_name(name):
     c = cush.CushApplication(name=name, namespace=Namespace())
     assert c.name == name
+    cush.CushApplication.del_application(name)
 
+#def test_init_cush(CushApplication):
+#    cush.init_cush(step=False)
 
+@pytest.mark.parametrize('CushApplication', ['init_user_test'], indirect=True)
+def test_init_user_ns(CushApplication):
+    CushApplication.init_user_namespace()
+    assert CushApplication.name == 'init_user_test'
 
-def test_init_cush(CushApplication):
-    cush.init_cush(step=False)
+def test_init_implementor_ns(CushApplication):
+    CushApplication.init_user_namespace()
+    CushApplication.init_implementor_namespace()
+    assert CushApplication.name == 'default'
 
+def test_reinit_implementor_ns(CushApplication):
+    CushApplication.init_user_namespace()
+    CushApplication.init_implementor_namespace()
+    assert CushApplication.name == 'default'
+
+#def test_fixture_cleanup(CushApplication):
+#    CushApplication.init_user_namespace()
+#    CushApplication.init_implementor_namespace()
+#    assert CushApplication.name == 'default'
+
+#def test_init_default_ns(CushApplication):
+#    CushApplication.init_default_namespace()
+#    assert CushApplication.name == 'default'
+#
+#def test_init_param_ns(CushApplication):
+#    CushApplication.init_param_namespace()
+#    assert CushApplication.name == 'default'
+#
+#def test_init_provider_ns(CushApplication):
+#    CushApplication.init_provider_namespace()
+#    assert CushApplication.name == 'default'
+#
+#def test_init_sdk_ns(CushApplication):
+#    CushApplication.init_provider_namespace()
+#    CushApplication.init_sdk_namespace()
+#    assert CushApplication.name == 'default'
