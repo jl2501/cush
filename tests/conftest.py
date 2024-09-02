591d5c6de636ba2f2d0883f33f0cb4292f457580
James Light <jl25019@gmail.com>
2024-09-02 13:32:56 -0400

    add cush application as a fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,13 @@ def CushApplication(request):
     try:
         app_name = request.param
     except AttributeError:
-        app_name = ''
+        app_name = 'default'
 
+    print(f"fixture creating {app_name}")
     c = cush.CushApplication(name=app_name, namespace=Namespace())
     yield c
-    c._applications.clear()
-    del c
 
+    print(f"fixture deleting {app_name}")
+    cush.CushApplication.del_application(app_name)
+    del c
+    print(f"len(cush.CushApplication._applications): {len(cush.CushApplication._applications)}")
